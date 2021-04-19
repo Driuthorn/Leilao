173df294379b00827f73b8445538a10310f6cc76
Driuthorn <Douglas_doAmaral@hotmail.com>
2021-04-19 01:36:52 -0300

    Inicializa o repositório contendo a inicialização da Api e as pastas bases .

---
+++ src/Backend/Leilao/Leilao.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Leilao.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var rng = new Random();
+            return rng.ToString();
+        }
+    }
+}
---
+++ src/Backend/Leilao/Leilao.Data.Database/Context/LeilaoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leilao.Data.Database.Context
+{
+    public class LeilaoContext : DbContext
+    {
+        public LeilaoContext(DbContextOptions<LeilaoContext> options) : base(options)
+        { }
+
+        public LeilaoContext()
+        { }
+    }
+}
